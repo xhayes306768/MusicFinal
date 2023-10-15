53bbd4da47d828e3847458c3d219fed8e2c17857
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-15 16:10:29 -0500

    Created the MusicContext class that inherits the DBContextb class.

--- MusicList2/Models/Music.cs
+++ MusicList2/Models/Music.cs
@@ -1,6 +1,26 @@
-﻿namespace MusicList2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicList2.Models
 {
     public class Music
     {
+        public int MusicId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a title.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter the artist.")]
+        public string Artist { get; set; }
+
+        [Required(ErrorMessage = "Please enter the year.")]
+        [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int Rating { get; set; } 
     }
+
+
 }
+
