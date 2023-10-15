b4f4c3a73f1d486eb315a160e1b620fd282dbd04
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-15 16:14:32 -0500

    Created 2 tables through MusicContext and seeded them with initial data.

--- MusicList2/Models/MusicContext.cs
+++ MusicList2/Models/MusicContext.cs
@@ -1,6 +1,29 @@
-﻿namespace MusicList2.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicList2.Models
 {
-    public class MusicContext
+    public class MusicContext:DbContext
     {
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options) { }
+
+        public DbSet<Music> Music { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Music>().HasData(
+                new Music { MusicId = 1,
+                    Title = "Song 1",
+                    Artist = "Artist 1",
+                    Year = 2000,
+                    Rating = 4 },
+
+                new Music { MusicId = 2,
+                    Title = "Song 2",
+                    Artist = "Artist 2",
+                    Year = 2010,
+                    Rating = 5 }
+            );
+        }
+
+
     }
 }
