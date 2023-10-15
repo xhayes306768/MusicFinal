74032a04be6ebbae8ce60bfe7611a5c7e146d669
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-15 12:34:21 -0500

    Add project files.

---
+++ MusicList2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MusicList2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
