691f2acfbb7d02934dda4babd9f8d24c5523e37b
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-15 16:07:46 -0500

    Created the music class.

--- MusicList2/Controllers/HomeController.cs
+++ MusicList2/Controllers/HomeController.cs
@@ -18,15 +18,7 @@ namespace MusicList2.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
+        
     }
 }
---
+++ MusicList2/Models/Music.cs
@@ -0,0 +1,6 @@
+﻿namespace MusicList2.Models
+{
+    public class Music
+    {
+    }
+}
